33f3398226ad0bc7d1ebab9d3b9025b682469035
carl.dunne <developerdunne@gmail.com>
2023-10-25 12:23:26 +0100

    Ensured winning number was secret

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -17,6 +16,7 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent =
       "Nice one bruva, correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
