c5a8f0eb0b034b8cd051620d96f203820680ba64
carl.dunne <developerdunne@gmail.com>
2023-10-25 11:35:08 +0100

    Added high and low scores functions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-'use strict';
+"use strict";
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".message").textContent = "Correct Number!";
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// document.querySelector(".guess").value = 4;
+// console.log(document.querySelector(".guess").value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent =
+      "Nice one bruva, correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too High Bro!";
+    score = score - 1;
+    document.querySelector(".score").textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too low mate!";
+    score = score - 1;
+    document.querySelector(".score").textContent = score;
+  }
+});
